aaa03df6bd83558c6d8c02c9d2a491538bbe6cf5
Tim Fechner <tim.b.f@gmx.de>
2014-09-30 13:07:07 +0200

    Settings better configurable

--- scripts/forum-beautifier.user.js
+++ scripts/forum-beautifier.user.js
@@ -4,33 +4,38 @@
 // @include     http://de110.die-staemme.de/game.php?*&screen=forum*
 // @updateURL   https://github.com/HWDexperte/dstools/raw/master/scripts/forum-beautifier.user.js
 // @downloadURL https://github.com/HWDexperte/dstools/raw/master/scripts/forum-beautifier.user.js
-// @version     1.1s
+// @version     1.2
 // @grant       none
 // ==/UserScript==
 
-elements = document.getElementsByClassName('forum');
 
-for (i=0; i < elements.length; i++) {
-  tag = elements[i];
-  tag.style.padding = '0px 4px 0px 4px';
-  tag.style.borderRadius = '4px';
+// === HERE YOU CAN MODIFY THE SETTINGS ===
+var new_post_color = "#88ACDB";
+var box_radius     = "3px";
+// ========================================
+
+
+function modify_boxes() {
+    elements = document.getElementsByClassName('forum');
+    for (i=0; i < elements.length; i++) {
+        tag = elements[i];
+        tag.style.padding = '0px 4px 0px 4px';
+        tag.style.borderRadius = box_radius;
+    }
 }
-
-
-elements = document.getElementsByTagName('img');
-
-for (i=0; i < elements.length; ) {
-  image = elements[i];
-  if (image.title.search(/Neuer\ Beitrag/) > -1) {
-    image.parentNode.parentNode.style.backgroundColor = "#ff0000";
-    image.remove();
-  } else {
-    i++;
-  }
+function highlight_new() {
+    elements = document.getElementsByTagName('img');
+    for (i=0; i < elements.length; ) {
+        image = elements[i];
+        if (image.title.search(/Neuer\ Beitrag/) > -1) {
+            image.parentNode.parentNode.style.backgroundColor = new_post_color;
+            image.remove();
+        } else { i++; }
+    }
 }
-
 function main() {
-
+    modify_boxes();
+    highlight_new();
 }
 
 main();
